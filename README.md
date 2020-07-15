6a82414ded264cce883bc96141237bdbd6a528fb
Robinson Aizprua <robinson.aizprua@ge.com>
2020-07-15 01:07:00 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # redis-http
+
+simple health check endpoint over http for redis
+
+use case scenario
+
+aws target group health check only support http and http healthchecks protocols. This will allow you to bypass that
+
+# redis-http install
+
+docker pull simcoder/redis-http:latest
+
+or clone this repo and run
+
+docker build -t redis-http .
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => {
     const result = process.execSync("redis-cli ping").toString();
-    return res.status(200).send({ status: result.includes("PONG") ? 'UP' : 'DOWN' });
+    return result.includes("PONG") ? res.status(200).send({ status:  'UP' }) : res.status(503).send({ status:  'DOWN' });
 });
 
 app.listen(port, () => console.log(`server started at port ${port}`))
